d673c65c03c41a14232a2c56eee44b547b47c4a5
Ivan Misuno <im@popappfactory.com>
2017-02-06 23:00:53 +0100

    changes after local $ rake install

--- swiftgen-cli/main.swift
+++ swiftgen-cli/main.swift
@@ -10,7 +10,7 @@ import PathKit
 
 // MARK: Common
 
-let templatesRelativePath = (Bundle.main.resourcePath ?? ".") + "/templates"
+let templatesRelativePath = "../templates"
 
 func templateOption(prefix: String) -> Option<String> {
   return Option<String>(
